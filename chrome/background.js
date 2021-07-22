fe03cf2c4a7b6f0ce75a07d29f31cb80dadcb22d
Allen Zhou <allenzhou@Allen-MacBook.local>
2021-07-22 22:03:04 +0800

    siyuan-utils 新增「闪念」功能

--- chrome/background.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	'use strict';
-
-	chrome.runtime.onInstalled.addListener((()=>{console.log("[crx3] background.js is running..."),console.log("background import success...");})),chrome.runtime.onMessage.addListener((function(n,s,e){n.msg&&e({msg:"后台收到！"+n.msg});}));
-
-}());
